a042ead214539661d56fb5b24f68c967eedd46a8
Ebuka Kingsley <keccweb04@gmail.com>
2022-07-01 02:20:42 +0100

    added the product page and implemented some form validations

---
+++ index.js
@@ -0,0 +1,17 @@
+let get = document.getElementById('pass1')
+let get1 = document.getElementById('pass2')
+let bod = document.getElementById('log')
+
+function verify(){
+    alert('Please check your mail to confirm your email address')
+}
+
+// if (get === get1){
+//     alert('thanks')
+// } else{
+//     alert("passwords don't match")
+// } 
+
+bod.addEventListener('click',function fff(){
+    alert('website is ready')
+})
